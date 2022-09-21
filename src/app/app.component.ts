7c28b60da20fe1299ed6cf317179f56c07071422
justasti <justas.tirevicius@gmail.com>
2022-09-21 11:07:27 +0300

    feat: basic setup i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ehealth-FE';
+export class AppComponent implements OnInit {
+  constructor(
+    private translate: TranslateService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  defaultLang: string = this.getLangCookie() || 'en';
+
+  onLangChange(lang: string): void {
+    this.translate.use(lang);
+    document.cookie = `lang=${lang}`;
+    this.router.navigate([], { relativeTo: this.route, queryParams: { lang } });
+  }
+
+  getLangCookie(): string | undefined {
+    const cookies = document.cookie.split(';');
+    let langCookie;
+    for (let cookie of cookies) {
+      if (cookie.includes('lang')) {
+        langCookie = cookie.substring(5);
+      }
+    }
+    return langCookie;
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((data) => data);
+    this.translate.setDefaultLang(this.defaultLang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
